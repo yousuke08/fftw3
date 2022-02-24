3578a61f321542c3270525fa5b51d85682bde1cd
yosukeito.ac <yosukeito@stn.nagaokaut.ac.jp>
2022-02-24 17:22:29 +0900

    [add] CSVのpathを選択して波形を表示させる部分を実装
    CSVのpathを選択して波形を表示させる部分を実装した
    パスのバリデーションは未実装(存在しないバスを選択するとAddは押せるがアプリが落ちる)

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+#plot用の変数
+CSVpath = ""
+
 # GUIがぼやける現象を防ぐための関数
 def make_dpi_aware():
   import ctypes
@@ -18,47 +21,59 @@ def draw_figure(canvas, figure):
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
 
+def loadCSV(targetCSV):
+    npArray = np.genfromtxt(targetCSV, delimiter=",", encoding='utf8', dtype='float')
+    print(npArray)
+    x = npArray.T[0]
+    y = npArray.T[1]
+    print(x)
+    print(y)
+    ax.plot(x, y, alpha=0.4)
+
 # レイアウト作成
-layout = [[sg.Text('Embed Matplotlib Plot')],
-          [sg.Canvas(key='-CANVAS-')],
-          [sg.Button("Add"), sg.Button("Clear")]]
+mainWindow = [[sg.Text("ファイル選択"), sg.Input(key="-FILEPATH-", enable_events=True), sg.FileBrowse()],
+              [sg.Canvas(key='-CANVAS-'), sg.Canvas(key='-CANVAS2-')],
+              [sg.Button("Add", key='-RUN-', disabled=True), sg.Button("Clear")]]
+
+subWindow = [[sg.Text('Embed Matplotlib Plot')]]
+
+layout = [[sg.Column(mainWindow, element_justification='c'),
+          sg.Column(subWindow, element_justification='c')]]
 
 # windowを作成する．finalize=Trueにする必要がある．
 window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True, element_justification='center', font='Monospace 18')
 
 # 埋め込む用のfigを作成する．
-fig = plt.figure(figsize=(5, 4))
+fig = plt.figure(figsize=(4, 3))
 ax = fig.add_subplot(111)
 ax.set_ylim(-10, 10)
 
+
 # figとCanvasを関連付ける．
 fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+fig_agg2 = draw_figure(window['-CANVAS2-'].TKCanvas, fig)
 
 # イベントループ
 while True:
     event, values = window.read()
-    print(event, values)
-    # sg.Print(event, values)
+    print(event)
 
     if event in (None, "Cancel"):
         break
 
-    elif event == "Add":
-        # 適当なプロット用データ作成
-        t = np.linspace(0, 7, 100)
-        afreq = np.random.randint(1, 10)
-        amp = np.random.randint(1, 10)
-        y = amp * np.sin(afreq * t)
-
-        # プロット
-        ax.plot(t, y, alpha=0.4)
-
+    elif event == "-RUN-":
+        loadCSV(values["-FILEPATH-"])
         # 変更を加えたあと，fig_agg.draw()で変更を反映させる．
         fig_agg.draw()
-
+        fig_agg2.draw()
+    
     elif event == "Clear":
         ax.cla()
         fig_agg.draw()
+        fig_agg2.draw()
+
+    elif event == "-FILEPATH-":
+        window.find_element('-RUN-').Update(disabled=False)
 
 # ウィンドウを閉じる．
 window.close()
