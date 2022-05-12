164ec27c54e983d391a720482067fb9f34ebf702
yosukeito.ac <yosukeito@stn.nagaokaut.ac.jp>
2022-05-12 17:58:36 +0900

    [change] FFTの表示をbarからscattarに変更した(高速化のため)

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class data:
         self.waveFig.subplots_adjust(top=0.98, right=0.98, bottom=0.15)
         self.waveAx = self.waveFig.add_subplot(111)
         #self.waveAx.plot(self.timeData, self.waveData, marker='.')
-        self.waveAx.plot(self.timeData, self.waveData, marker='.')
+        self.waveAx.scatter(self.timeData, self.waveData, marker='.')
         self.waveAx.set_xlim(0, self.dataLen * self.samplingTime)
         self.waveAx.xaxis.set_major_locator(ptick.MaxNLocator(5))
         self.waveAx.xaxis.set_major_formatter(ptick.ScalarFormatter(useMathText=True))
@@ -41,7 +41,7 @@ class data:
         self.freqAx = self.freqFig.add_subplot(111)
         self.freqAx.xaxis.set_major_locator(ptick.MaxNLocator(5))
         self.freqAx.set_xlim(0, (self.dataLen) / 2.0)
-        self.freqAx.bar(self.freqData, self.freqDataAbs)
+        self.freqAx.scatter(self.freqData, self.freqDataAbs, marker='.')
 
     def drawWaveFig(self):
         if window['-waveCanvas-'].TKCanvas.children:
@@ -73,13 +73,13 @@ sg.theme('Light Blue 2')
 
 #frameTime = [[sg.Image(filename='', key='-imageTime-', size=(400, 300))]]
 frameTime = [[sg.Canvas(key='-waveCanvas-', size=(400, 300))]]
-frameFreq = [[sg.Canvas(key='-freqCanvas-', size=(400, 300))],[sg.InputText(key='-freqRange-', enable_events=True, justification='right'), sg.Button('Apply', key='-reloadFreqRange-', disabled=True)]]
+frameFreq = [[sg.Canvas(key='-freqCanvas-', size=(400, 300))],[sg.Text('表示最大周波数'), sg.InputText(key='-freqRange-', enable_events=True, justification='right', size=(10,1)), sg.Button('Apply', key='-reloadFreqRange-', disabled=True)]]
 
-layout = [[sg.Text('My one-shot window.')],
+layout = [[sg.Text('Ver_0.1', justification='right')],
           [sg.Text("ファイル選択"), sg.InputText(key="-FILEPATH-", enable_events=True), sg.FileBrowse()],
           [sg.Text('サンプリング周波数[us]'), sg.InputText(key='-SETTS-', enable_events=True, disabled=True), sg.Button('Run', key='-RUN-', disabled=True)],
           [sg.Button('Display',key='-display-',disabled=True), sg.Cancel('Close',key='Cancel')],
-          [sg.Table([[1,2,3], [1,2,3]], ['Time', 'Data'], num_rows=18), sg.Frame(title='Waveform', layout=frameTime, border_width=7), sg.Frame(title='FFT', layout=frameFreq, border_width=7)],
+          [sg.Table([[0,0], [0,0]], ['Time', 'Data'], num_rows=18, ), sg.Frame(title='Waveform', layout=frameTime, border_width=7), sg.Frame(title='FFT', layout=frameFreq, border_width=7)],
          ]
 
 window = sg.Window('Window Title', layout, size=(1000,600), return_keyboard_events=True)
@@ -92,11 +92,15 @@ while True:
 
     elif event == '-display-':
         simpleSin.refreshTimeData()
+        print('finish:refreshTimeData\nbegin:makeWaveFig')
         simpleSin.makeWaveFig()
+        print('finish:makeWaveFig\nbegin:runFFT')
         simpleSin.runFFT()
+        print('finish:runFFT\nbegin:refreshFreqData')
         simpleSin.refreshFreqData()
+        print('finish:refreshFreqData\nbegin:makeFreqFig')
         simpleSin.makeFreqFig()
-
+        print('finish:makeFreqFig')
         window['-waveCanvas-'].update(data=simpleSin.drawWaveFig())
         window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
     
@@ -115,10 +119,10 @@ while True:
         simpleSin.refreshTimeData()
     
     elif event == '-freqRange-':
-        simpleSin.freqAx.set_xlim(0, int(values['-freqRange-']))
         window.find_element('-reloadFreqRange-').Update(disabled=False)
 
     elif event == '-reloadFreqRange-':
+        simpleSin.freqAx.set_xlim(0, int(values['-freqRange-']))
         window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
 
 
