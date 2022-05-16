c8f19d06e0fb0b1b49d859862a0d32393b38f248
yosukeito.ac <yosukeito@stn.nagaokaut.ac.jp>
2022-05-16 22:43:09 +0900

    [add] FFT波形の縦軸を振幅，実効値，db値で選択できるように変更
    まだ縦軸スケール変更にバグがある

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class data:
 
     def makeWaveFig(self):
         self.waveFig = plt.figure(dpi=100, figsize=(4, 3))
-        self.waveFig.subplots_adjust(top=0.98, right=0.98, bottom=0.15)
+        self.waveFig.subplots_adjust(top=0.9, right=0.98, bottom=0.15)
         self.waveAx = self.waveFig.add_subplot(111)
         #self.waveAx.plot(self.timeData, self.waveData, marker='.')
         self.waveAx.scatter(self.timeData, self.waveData, marker='.')
@@ -37,11 +37,11 @@ class data:
 
     def makeFreqFig(self):
         self.freqFig = plt.figure(dpi=100, figsize=(4, 3))
-        self.freqFig.subplots_adjust(top=0.98, right=0.98, bottom=0.15)
+        self.freqFig.subplots_adjust(top=0.9, right=0.98, bottom=0.15)
         self.freqAx = self.freqFig.add_subplot(111)
         self.freqAx.xaxis.set_major_locator(ptick.MaxNLocator(5))
         self.freqAx.set_xlim(0, (self.dataLen) / 2.0)
-        self.freqAx.scatter(self.freqData, self.freqDataAbs, marker='.')
+        self.freqAx.scatter(self.freqData, self.freqDataAbsDisp, marker='.')
 
     def drawWaveFig(self):
         if window['-waveCanvas-'].TKCanvas.children:
@@ -65,20 +65,28 @@ class data:
         a[:] = self.waveData
         b  = pyfftw.interfaces.numpy_fft.fft(a)
         self.compFreqData = np.fft.fft(a)
-        self.freqDataAbs = np.abs(self.compFreqData)/(self.dataLen)
+        self.freqDataAbs = np.abs(self.compFreqData) / (self.dataLen)
+        self.freqDataAbsDisp = self.freqDataAbs
         self.freqDataAng = np.angle(self.compFreqData, deg=True)
     
     def exportCSV(self):
-        np.savetxt('./output.csv', np.stack([np.array(self.freqData), self.freqDataAbs]).T, delimiter=',')
+        np.savetxt('./output.csv', np.stack([np.array(self.freqData), self.freqDataAbsDisp]).T, delimiter=',')
         
+    def yAxisLog(self):
+        self.freqDataAbsDisp = 20 * np.log10(self.freqDataAbs)
 
+    def yAxisAmp(self):
+        self.freqDataAbsDisp = self.freqDataAbs
+        
+    def yAxisRMS(self):
+        self.freqDataAbsDisp = self.freqDataAbs/np.sqrt(2)
 
 
 sg.theme('Light Blue 2')
 
 #frameTime = [[sg.Image(filename='', key='-imageTime-', size=(400, 300))]]
 frameTime = [[sg.Canvas(key='-waveCanvas-', size=(400, 300))]]
-frameFreq = [[sg.Canvas(key='-freqCanvas-', size=(400, 300))],[sg.Text('表示最大振幅'), sg.InputText(key='-ampRange-', enable_events=True, justification='right', size=(5,1)), sg.Button('Apply', key='-reloadAmpRange-', disabled=True), sg.Text('表示最大周波数'), sg.InputText(key='-freqRange-', enable_events=True, justification='right', size=(5,1)), sg.Button('Apply', key='-reloadFreqRange-', disabled=True)]]
+frameFreq = [[sg.Radio('Amp.', group_id='yAxis', key='-yAxisAmp-', enable_events=True,default=True, disabled=True), sg.Radio('RMS', group_id='yAxis', key='-yAxisRMS-', enable_events=True, disabled=True), sg.Radio('Amp.dbV', group_id='yAxis', key='-yAxisLog-', enable_events=True, disabled=True)], [sg.Canvas(key='-freqCanvas-', size=(400, 300))],[sg.Text('表示最大振幅'), sg.InputText(key='-ampRange-', enable_events=True, justification='right', size=(5,1)), sg.Button('Apply', key='-reloadAmpRange-', disabled=True, enable_events=True), sg.Text('表示最大周波数'), sg.InputText(key='-freqRange-', enable_events=True, justification='right', size=(5,1)), sg.Button('Apply', key='-reloadFreqRange-', disabled=True)]]
 
 layout = [[sg.Text('Ver_0.1', justification='right')],
           [sg.Text("ファイル選択"), sg.InputText(key="-FILEPATH-", enable_events=True), sg.FileBrowse()],
@@ -115,6 +123,9 @@ while True:
         window['-waveCanvas-'].update(data=simpleSin.drawWaveFig())
         window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
         window.find_element('-saveCSV-').Update(disabled=False)
+        window.find_element('-yAxisLog-').Update(disabled=False)
+        window.find_element('-yAxisAmp-').Update(disabled=False)
+        window.find_element('-yAxisRMS-').Update(disabled=False)
     
     elif event == '-freqRange-':
         window.find_element('-reloadFreqRange-').Update(disabled=False)
@@ -133,5 +144,22 @@ while True:
     elif event == '-saveCSV-':
         simpleSin.exportCSV()
 
+    elif event == '-yAxisLog-':
+        simpleSin.yAxisLog()
+        simpleSin.makeFreqFig()
+        simpleSin.drawFreqFig()
+        window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
+
+    elif event == '-yAxisAmp-':
+        simpleSin.yAxisAmp()
+        simpleSin.makeFreqFig()
+        simpleSin.drawFreqFig()
+        window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
+
+    elif event == '-yAxisRMS-':
+        simpleSin.yAxisRMS()
+        simpleSin.makeFreqFig()
+        simpleSin.drawFreqFig()
+        window['-freqCanvas-'].update(data=simpleSin.drawFreqFig())
 
 window.close()
